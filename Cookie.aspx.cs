09129cceb4cdd9d2ac5c3cb14da871d4aa5da3ed
Andrea1603Garcia <melissagarciaromero57@gmail.com>
2025-03-04 14:05:15 -0600

    Primer Commit

---
+++ Cookie.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Asp_E4_4955454
+{
+    public partial class Cookie : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnCreateArray_Click(object sender, EventArgs e)
+        {
+            String[] txtCategoryArray = new string[5];
+            txtCategoryArray[0] = "Footwear - Men's";
+            txtCategoryArray[1] = "Clothing - Men's";
+            txtCategoryArray[2] = "Racquets";
+            txtCategoryArray[3] = "Footwear - Women's";
+            txtCategoryArray[4] = "Clothing - Women's";
+
+            Session["txtCategoryArray"] = txtCategoryArray;
+
+            Response.Redirect("cookis2.aspx");
+        }
+    }
+}
